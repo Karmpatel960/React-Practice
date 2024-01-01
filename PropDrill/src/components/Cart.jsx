616031e501294bd3b0eba57a6d39d1d48ee35113
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2024-01-01 23:34:15 +0530

    prop drill added

---
+++ PropDrill/src/components/Cart.jsx
@@ -0,0 +1,45 @@
+import { useContext } from 'react'
+import { CartContext } from './shopping-cart-context'
+
+export default function Cart({ items, onUpdateItemQuantity }) {
+  const CartCtx = useContext(CartContext)
+  const totalPrice = CartCtx.items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  )
+  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`
+
+  return (
+    <div id='cart'>
+      {CartCtx.items.length === 0 && <p>No items in cart!</p>}
+      {CartCtx.items.length > 0 && (
+        <ul id='cart-items'>
+          {CartCtx.items.map((item) => {
+            const formattedPrice = `$${item.price.toFixed(2)}`
+
+            return (
+              <li key={item.id}>
+                <div>
+                  <span>{item.name}</span>
+                  <span> ({formattedPrice})</span>
+                </div>
+                <div className='cart-item-actions'>
+                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                    -
+                  </button>
+                  <span>{item.quantity}</span>
+                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                    +
+                  </button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      )}
+      <p id='cart-total-price'>
+        Cart Total: <strong>{formattedTotalPrice}</strong>
+      </p>
+    </div>
+  )
+}
---
+++ PropDrill/src/components/Product.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react'
+
+import { CartContext } from './shopping-cart-context'
+
+export default function Product({
+  id,
+  image,
+  title,
+  price,
+  description,
+  onAddToCart,
+}) {
+  const cartCtx = useContext(CartContext)
+
+  return (
+    <article className='product'>
+      <img src={image} alt={title} />
+      <div className='product-content'>
+        <div>
+          <h3>{title}</h3>
+          <p className='product-price'>${price}</p>
+          <p>{description}</p>
+        </div>
+        <p className='product-actions'>
+          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+        </p>
+      </div>
+    </article>
+  )
+}
